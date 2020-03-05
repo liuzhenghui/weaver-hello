adf51273252e0232eb2fbdb3a364f02ef990ce55
zheng <liu.zhenghui@outlook.com>
2020-03-05 10:17:06 +0800

    init

---
+++ ecology/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zheng on 2020/3/5.
+ */
+public class Main {
+    static public void main(String[] s) {
+        System.out.println(" Main ");
+    }
+}
---
+++ test/src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zheng on 2020/3/5.
+ */
+public class Test {
+    static public void main(String[] s) {
+        System.out.println(" Test ");
+    }
+}
